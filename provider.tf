ca3142d9b44b0812b502357c0552e339a8ca2f94
Matt Eng <matthew_eng@timeinc.com>
2020-03-25 22:41:47 -0400

    Use credentials over Access key

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  region     = var.AWS_REGION
   shared_credentials_file = var.AWS_SHARED_CREDENTIALS_FILE
   profile                 = var.AWS_PROFILE
   region                  = var.AWS_REGION
