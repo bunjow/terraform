590a565957871a8362a72a6f1935022efd0e044a
Matt Eng <matthew_eng@timeinc.com>
2020-03-25 22:45:01 -0400

    fixing variables

--- vars.tf
+++ vars.tf
@@ -16,3 +16,14 @@ variable "AMIS" {
   }
 }
 
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
